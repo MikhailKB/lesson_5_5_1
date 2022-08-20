8489def5f86e8f3893e2498c9f3a54b58a1db26f
Михаил Козин <Vasago85@mail.ru>
2022-08-21 02:34:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+
+namespace lesson_5_5_1
+
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param> любой введённый случайный текст
+        /// <returns></returns>
+        static string[] ArrayCreation(string text) // создание массива
+        {
+            string[] words = text.Split(' '); // разбивает строку на максимальное число подстрок на основе указанного символа-разделителя
+            return words;
+        }
+        static string[] DataOutput(string[] words) // вывод данных
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                WriteLine($"{words[i]} ");
+            }
+            return words;
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("Введите случайное предложение: ");
+            string text = ReadLine();
+            ArrayCreation(text);
+            string[] words = ArrayCreation(text);
+            DataOutput(words);
+            ReadKey();
+        }
+    }
+}
